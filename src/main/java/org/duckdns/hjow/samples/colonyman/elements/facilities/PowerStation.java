4b05a9b051144730f12203ab69097d0df7bf8689
HJOW <hujinone22@naver.com>
2025-09-23 22:36:02 +0900

    콜로니 프로젝트 - 파워스테이션 추상화

--- src/main/java/org/duckdns/hjow/samples/colonyman/elements/facilities/PowerStation.java
+++ src/main/java/org/duckdns/hjow/samples/colonyman/elements/facilities/PowerStation.java
@@ -1,57 +1,19 @@
 package org.duckdns.hjow.samples.colonyman.elements.facilities;
 
-import org.duckdns.hjow.commons.json.JsonObject;
-import org.duckdns.hjow.samples.colonyman.ColonyManager;
-import org.duckdns.hjow.samples.colonyman.elements.Citizen;
 import org.duckdns.hjow.samples.colonyman.elements.City;
 import org.duckdns.hjow.samples.colonyman.elements.Colony;
 
-public class PowerStation extends DefaultFacility {
+public class PowerStation extends PowerPlant {
     private static final long serialVersionUID = 4079646708867981024L;
-    protected String name = "발전소_" + ColonyManager.generateNaturalNumber();
-    protected int capacity = 100;
-    
-    @Override
-    public String getType() {
-        return "PowerStation";
-    }
-    @Override
-    public String getName() {
-        return name;
-    }
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public void oneSecond(int cycle, City city, Colony colony, int efficiency100) {
-        super.oneSecond(cycle, city, colony, efficiency100);
-        
-        // Do nothing on PowerStation (implemented on City class)
-    }
-    @Override
-    public void fromJson(JsonObject json) {
-        setName(json.get("name").toString());
-        key = Long.parseLong(json.get("key").toString());
-        setHp(Integer.parseInt(json.get("hp").toString()));
-        setCapacity(Integer.parseInt(json.get("capacity").toString()));
+
+    protected String getDefaultNamePrefix() {
+        return "발전소";
     }
 
-    @Override
-    public JsonObject toJson() {
-        JsonObject json = new JsonObject();
-        json.put("type", getType());
-        json.put("name", getName());
-        json.put("key", new Long(getKey()));
-        json.put("hp", new Long(getHp()));
-        json.put("capacity", new Integer(getCapacity()));
-        
-        return json;
+    protected int getDefaultCapacity() {
+        return 100;
     }
-    @Override
-    public int increasingCityMaxHP() {
-        return 1;
-    }
+    
     @Override
     public String getStatusDescription(City city, Colony colony) {
         return ""; // TODO
@@ -63,17 +25,7 @@ public class PowerStation extends DefaultFacility {
     public int getPowerGenerate(Colony col, City city) {
         return getCapacity();
     }
-    @Override
-    public int getCapacity() {
-        return capacity;
-    }
-    public void setCapacity(int c) {
-        this.capacity = c;
-    }
-    @Override
-    public int getComportGrade() {
-        return 0;
-    }
+
     @Override
     public int getMaxHp() {
         return 1000;
@@ -85,16 +37,6 @@ public class PowerStation extends DefaultFacility {
     @Override
     public int getWorkerCapacity() {
         return 2;
-    }
-    @Override
-    public int getWorkerSuitability(Citizen citizen) {
-        int point = 3;
-        if(citizen.getCarisma()     >= 3) point += 1;
-        if(citizen.getAgility()     >= 6) point += 2;
-        if(citizen.getStrength()    >= 6) point += 2;
-        if(citizen.getIntelligent() >= 6) point += 2;
-        
-        return point;
     }
     
     public static String getFacilityName() {
