fa5e7ef64ad9a7f67ba3f6bef86a93ac55d5600c
HJOW <hjow@djict.co.kr>
2025-09-25 13:31:10 +0900

    콜로니 프로젝트 - 백업 암호화 기능 보강

--- src/main/java/org/duckdns/hjow/samples/colonyman/elements/ColonyBackup.java
+++ src/main/java/org/duckdns/hjow/samples/colonyman/elements/ColonyBackup.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Vector;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.duckdns.hjow.commons.json.JsonArray;
@@ -60,6 +61,9 @@ public class ColonyBackup {
         if(security == "null") security = "";
         security = security.trim();
         
+        String strVer = json.get("backupver").toString().trim();
+        int ver = Integer.parseInt(strVer);
+        
         JsonArray list = null;
         if(security.equals("")) {
             list = (JsonArray) json.get("list");
@@ -71,18 +75,22 @@ public class ColonyBackup {
                 byte[] decoded = HexUtil.decode(hexStr);
                 hexStr = null;
                 
+                // 비밀번호 해싱
+                String padded = getHashLeftSalt(ver) + password + getHashRightSalt(ver);
                 MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                byte[] pwdigested = digest.digest(password.getBytes("UTF-8"));
+                byte[] pwdigested = digest.digest(padded.getBytes("UTF-8"));
                 String pwencoded  = HexUtil.encode(pwdigested).substring(0, 32);
                 pwdigested = null;
                 
+                // 복호화
                 SecretKeySpec scKeySpec = new SecretKeySpec(pwencoded.getBytes("UTF-8"), "AES");
-                Cipher cipher = Cipher.getInstance("AES");
-                cipher.init(Cipher.DECRYPT_MODE, scKeySpec);
+                IvParameterSpec iv = new IvParameterSpec(getIv(ver).getBytes("UTF-8"));
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, scKeySpec, iv);
                 byte[] ciphered = cipher.doFinal(decoded);
                 decoded = null;
                 
-                String decryptedString = new String(ciphered, "UTF-8");
+                String decryptedString = new String(ciphered, "UTF-8").trim();
                 ciphered = null;
                 cipher = null;
                 
@@ -116,6 +124,7 @@ public class ColonyBackup {
         json.put("desc", getDescription());
         json.put("created", new Long(getCreated()));
         json.put("security", "");
+        json.put("backupver", new Integer(getBackupVersion()));
         
         JsonArray list = new JsonArray();
         for(Colony c : getColonies()) { list.add(c.toJson()); }
@@ -134,14 +143,34 @@ public class ColonyBackup {
             if(security.equalsIgnoreCase("AES")) {
                 byte[] binaries = list.toJSON().getBytes("UTF-8");
                 
+                // byte 자리수를 16 배수로 맞추기
+                int nowLen = binaries.length;
+                int mods   = nowLen % 16;
+                
+                byte[] buffer = new byte[nowLen + (16-mods)];
+                for(int idx=0; idx<buffer.length; idx++) {
+                    if(idx < binaries.length) buffer[idx] = binaries[idx];
+                    else                      buffer[idx] = (byte) ' ';
+                }
+                binaries = buffer;
+                buffer = null;
+                
+                // 버전
+                int ver = getBackupVersion();
+                json.put("backupver", new Integer(ver));
+                
+                // 비밀번호 해싱
+                String padded = getHashLeftSalt(ver) + password + getHashRightSalt(ver);
                 MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                byte[] pwdigested = digest.digest(password.getBytes("UTF-8"));
+                byte[] pwdigested = digest.digest(padded.getBytes("UTF-8"));
                 String pwencoded  = HexUtil.encode(pwdigested).substring(0, 32);
                 pwdigested = null;
                 
+                // 암호화
                 SecretKeySpec scKeySpec = new SecretKeySpec(pwencoded.getBytes("UTF-8"), "AES");
-                Cipher cipher = Cipher.getInstance("AES");
-                cipher.init(Cipher.ENCRYPT_MODE, scKeySpec);
+                IvParameterSpec iv = new IvParameterSpec(getIv(ver).getBytes("UTF-8"));
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, scKeySpec, iv);
                 byte[] ciphered = cipher.doFinal(binaries);
                 list = null;
                 binaries = null;
@@ -197,5 +226,22 @@ public class ColonyBackup {
     
     public void setCreated(Date created) {
         this.created = created.getTime();
+    }
+    
+    public int getBackupVersion() { return 1; }
+    
+    public String getHashLeftSalt(int version) {
+        if(version == 1) return "ABS";
+        return "";
+    }
+    
+    public String getHashRightSalt(int version) {
+        if(version == 1) return "XYH";
+        return "";
+    }
+    
+    public String getIv(int version) {
+        if(version == 1) return "IVS4567890123456";
+        return "IV34567890123456";
     }
 }
